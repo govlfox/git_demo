452216b90c109fb202ae4734292aad0c413315f1
govlfox <92joker@naver.com>
2026-04-24 10:43:09 +0900

    더미메서드 추가

--- src/main/java/com/example/git_demo/GitDemoApplication.java
+++ src/main/java/com/example/git_demo/GitDemoApplication.java
@@ -9,5 +9,16 @@ public class GitDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(GitDemoApplication.class, args);
     }
-냥
+    public void sayHello() {
+        System.out.println("Hello from Demo1Application!");
+    }
+
+    public int addNumbers(int a, int b) {
+        return a + b;
+    }
+
+    public String getStatus() {
+        return "Application is running";
+    }
+
 }
